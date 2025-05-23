710f9888548e24f329afb621c4550a16d3e38d48
Rohit Zirmute <rohitzirmute821@gmail.com>
2025-05-23 22:19:45 +0530

    Added Documentation

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)  # Increased for salt
+    password = db.Column(db.String(256), nullable=False)
     role = db.Column(db.Enum(Role), default=Role.VIEWER)
 
 class Event(db.Model):
